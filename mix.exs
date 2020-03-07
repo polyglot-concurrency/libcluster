bd1bb3859f1dc7e74c7f94912f724fa88fd53024
José Albert Cruz Almaguer <jalbertcruz@gmail.com>
2020-03-07 23:45:02 +0100

    Consul DNS added

--- mix.exs
+++ mix.exs
@@ -5,25 +5,26 @@ defmodule Cluster.Mixfile do
   @source_url "https://github.com/bitwalker/libcluster"
 
   def project do
-    [app: :libcluster,
-     version: @version,
-     elixir: "~> 1.5",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     description: "Automatic Erlang cluster formation and management for Elixir/Erlang applications",
-     package: package(),
-     docs: docs(),
-     deps: deps(),
-     elixirc_paths: elixirc_paths(Mix.env),
-     dialyzer: [
+    [
+      app: :libcluster,
+      version: @version,
+      elixir: "~> 1.5",
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      description: "Automatic Erlang cluster formation and management for Elixir/Erlang applications",
+      package: package(),
+      docs: docs(),
+      deps: deps(),
+      elixirc_paths: elixirc_paths(Mix.env),
+      dialyzer: [
         flags: ~w(-Wunmatched_returns -Werror_handling -Wrace_conditions -Wno_opaque -Wunderspecs)
-     ],
-     preferred_cli_env: [
-       vcr: :test,
-       "vcr.delete": :test,
-       "vcr.check": :test,
-       "vcr.show": :test,
-     ]
+      ],
+      preferred_cli_env: [
+        vcr: :test,
+        "vcr.delete": :test,
+        "vcr.check": :test,
+        "vcr.show": :test,
+      ]
     ]
   end
 
@@ -32,27 +33,37 @@ defmodule Cluster.Mixfile do
   end
 
   defp deps do
-    [{:ex_doc, "~> 0.20.0", only: :dev},
-     {:dialyxir, "~> 0.5.1", only: :dev},
-     {:exvcr, "~> 0.10.3", only: :test},
-     {:jason, "~> 1.1.2"}]
+    [
+      {:ex_doc, "~> 0.20.0", only: :dev},
+      {:dialyxir, "~> 0.5.1", only: :dev},
+      {:exvcr, "~> 0.10.3", only: :test},
+      {:jason, "~> 1.1.2"}
+    ]
   end
 
   defp package do
-    [files: ["lib", "mix.exs", "README.md", "LICENSE.md"],
-     maintainers: ["Paul Schoenfelder"],
-     licenses: ["MIT"],
-     links: %{"GitHub": @source_url}]
+    [
+      files: ["lib", "mix.exs", "README.md", "LICENSE.md"],
+      maintainers: ["Paul Schoenfelder"],
+      licenses: ["MIT"],
+      links: %{
+        "GitHub": @source_url
+      }
+    ]
   end
 
   defp docs do
-    [main: "readme",
-     source_url: @source_url,
-     source_ref: "v#{@version}",
-     formatter_opts: [gfm: true],
-     extras: [
-       "README.md"
-     ]]
+    [
+      main: "readme",
+      source_url: @source_url,
+      source_ref: "v#{@version}",
+      formatter_opts: [
+        gfm: true
+      ],
+      extras: [
+        "README.md"
+      ]
+    ]
   end
 
   defp elixirc_paths(:test), do: ["lib", "test/support"]
